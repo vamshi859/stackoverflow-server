c5702153750205748e5f358d3dfc36fae3a19553
vamshi859 <vv517816@gmail.com>
2022-07-20 02:04:33 +0530

    make it better

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import userRoutes from './routes/user.js';
+import questionRoutes from './routes/Questions.js';
+import answerRoutes from './routes/Answers.js';
+import dotenv from "dotenv"
+const app = express();
+dotenv.config();
+app.use(express.json({limit: "30mb",extended: true}));
+app.use(express.urlencoded({limit: "30mb",extended:true}));
+app.use(cors());
+
+app.get('/',(req,res) => {
+  res.send("<h1>Vamshi Gujjuboina</h1>");
+})
+
+app.use('/user',userRoutes);
+app.use('/questions',questionRoutes);
+app.use('/answer',answerRoutes);
+
+const PORT = process.env.PORT || 5000;
+const DATABASE_URL = process.env.CONNECTION_URL
+
+mongoose.connect(DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(() => app.listen(PORT,() => console.log(`Server running on ${PORT}`)))
+  .catch((err) => console.log(err.message))
---
+++ routes/user.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import { signup,login } from '../controllers/auth.js';
+import { getAllUsers,updateProfile } from '../controllers/users.js';
+import auth from '../middlewares/auth.js';
+ const router = express.Router();
+
+ router.post('/signup',signup); //callback function is written controllers
+ router.post('/login',login);
+
+ router.get('/getAllUsers',getAllUsers);
+ router.patch('/update/:id',auth,updateProfile);
+
+ export default router
